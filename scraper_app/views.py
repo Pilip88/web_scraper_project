a731934774301bfa36a1d95b53e2db1ece477af3
Ivan Pilipović <pilipovicivan@yahoo.com>
2018-03-18 15:18:15 +0100

    Removed old view

--- scraper_app/views.py
+++ scraper_app/views.py
@@ -21,21 +21,3 @@ def home(request):
         request,
         'base.html',
         {'articles': articles})
-
-
-def get_data(request):
-    start_time = time.time()
-    #p1 = Process(target=scrapMotorsport)
-    #p2 = Process(target=scrapCrash)
-    #p1.start()
-    #p2.start()
-    #p1.join()
-    #p2.join()
-    articles = Article.objects.all().order_by(
-        "-article_date",
-        "-article_created")
-    print("Took %s seconds." % (time.time() - start_time))
-    return render(
-        request,
-        'data.html',
-        {'articles': articles})
